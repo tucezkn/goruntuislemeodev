a8058f478d6057d7aaf21088c1a29e2474d07313
tuğçe özkan <117521217+tucezkn@users.noreply.github.com>
2022-12-10 12:32:58 +0300

    Add files via upload

---
+++ odeevv.py
@@ -0,0 +1,17 @@
+dosya = open("data2.txt", "w")
+dosya.write("Name : gökçe\nSurname : sayın\nGender : female\nUsername : Wtfgokce\nJob : oyuncu")
+dosya.close()
+dosya = open("data2.txt", "r")
+string=dosya.read()
+s_string=string.split("\n")
+Dictionary={ 
+
+
+}
+types=[0,1,2,3,4]
+for i in types:
+    s2_string = s_string[i].split(":")
+    Dictionary[s2_string[0]]=s2_string[1]
+
+print(Dictionary)
+dosya.close()
