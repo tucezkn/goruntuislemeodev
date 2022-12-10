4339805f91c8596bcb05e7dd4ae6ff3878905c18
tuğçe özkan <117521217+tucezkn@users.noreply.github.com>
2022-12-10 21:48:25 +0300

    Add files via upload

--- asal sayi.py
+++ asal sayi.py
@@ -1,10 +1,16 @@
-a=int(input("bir sayı giriniz : "))
-if a > 1:
-    for i in range(2,a):
-        if(a%i)==0:
-            print(a, "Asal sayı değildir.")
-            break
-        else:
-            print(a, "Asal sayıdır.")    
-else:
-    print(a, "asal sayı değildir.")
+try:
+     a=int(input("bir sayı giriniz : "))
+
+
+     if a > 1:
+        for i in range(2,a):
+            if(a%i)==0:
+              print(a, "Asal sayı değildir.")
+              break
+            else:
+              print(a, "Asal sayıdır.")    
+     else: 
+      print(a, "asal sayı değildir.")
+
+except ValueError:
+    print("lütfen sayı giriniz")
